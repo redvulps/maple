2391cdfffe406520de97a6caa3edb632ad147415
Fabio Pereira <fabopereira@gmail.com>
2019-12-27 17:57:58 -0300

    Add base code for keyviewer

--- src/renderer/components/KeyViewer/index.tsx
+++ src/renderer/components/KeyViewer/index.tsx
@@ -1,14 +1,42 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Redis from 'redis';
+
+import { IRedisKey } from '../KeyTree';
 
 export interface IKeyViewerProps {
   redisInstance: Redis.RedisClient;
+  currentDatabase: number;
+  currentKey: IRedisKey | null;
 }
 
-const KeyViewer = (props: IKeyViewerProps) => (
-  <div>
-    keyviewer
-  </div>
-);
+const KeyViewer = ({currentKey}: IKeyViewerProps) => {
+  const [keyView, setKeyView] = useState<JSX.Element | null>(null);
+
+  const renderKey = () => {
+    if (keyView) {
+      const Component = keyView;
+
+      return (
+        <Component />
+      );
+    } else {
+      return (
+        'Loading...'
+      );
+    }
+  };
+
+  useEffect(() => {
+    if (currentKey) {
+
+    }
+  }, [currentKey]);
+
+  return (
+    <div>
+      { currentKey ? renderKey() : "No key selected" }
+    </div>
+  );
+}
 
 export default KeyViewer;
