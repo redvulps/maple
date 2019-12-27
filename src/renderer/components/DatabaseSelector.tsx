a5d712299616f83039ac2665e1e8eb058dc44a9e
Fabio Pereira <fabopereira@gmail.com>
2019-12-27 17:11:27 -0300

    Update database selector with proper functionality

--- src/renderer/components/DatabaseSelector.tsx
+++ src/renderer/components/DatabaseSelector.tsx
@@ -1,9 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Redis from 'redis';
 
-const DatabaseSelector = () => (
-  <div>
-    DatabaseSelector
-  </div>
-);
+export interface IDatabaseSelectorProps {
+  redisInstance: Redis.RedisClient;
+  currentDatabase: number;
+  onSelectDatabase: (databaseId: number) => void;
+}
+
+const DatabaseSelector = (props: IDatabaseSelectorProps) => {
+  const { currentDatabase, onSelectDatabase, redisInstance } = props;
+  const [databaseCount, setDatabaseCount] = useState(0);
+
+  useEffect(() => {
+    redisInstance.config("get", "databases", (err, result: any) => {
+      // TODO: Add error handler
+      setDatabaseCount(parseInt(result[1]));
+    });
+  }, []);
+
+  const databaseList = new Array(databaseCount).fill(0).map((_, index) => (
+    <option key={index} value={index} defaultChecked={index === currentDatabase}>Database {index}</option>
+  ));
+
+  return (
+    <div>
+      <select onChange={(e) => onSelectDatabase(parseInt(e.target.value))}>
+        { databaseList }
+      </select>
+    </div>
+  );
+};
 
 export default DatabaseSelector;
