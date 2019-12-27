8a881ad73b6754b468cdbe0af0f6a04a2c2a4f8e
Fabio Pereira <fabopereira@gmail.com>
2019-12-27 17:10:49 -0300

    Update new connection window with state, actions and favorite mock

--- src/renderer/components/NewConnection.tsx
+++ src/renderer/components/NewConnection.tsx
@@ -1,20 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const NewConnection = () => (
-  <div className="form vertical">
-    <div className="field">
-      <label>Host</label>
-      <input type="text" placeholder="127.0.0.1" />
+interface INewConnectionProps {
+  onConnect: (host:string, port:string) => void;
+  saveFavorite: (favoriteId: string, favoriteName: string, host: string, port: string) => void;
+  removeFavorite: (favoriteId: string) => void;
+  favoriteId: string;
+  favoriteName: string;
+  isConnecting: boolean;
+}
+
+const NewConnection = (props: INewConnectionProps) => {
+  const { onConnect, saveFavorite, removeFavorite, favoriteId, favoriteName } = props;
+  const isFavorite = !!favoriteId;
+
+  const [host, setHost] = useState("");
+  const [port, setPort] = useState("");
+
+  let favoriteActions;
+  const saveFavoriteAction = () => {
+    saveFavorite(favoriteId, favoriteName, host, port);
+  };
+
+  if (isFavorite) {
+    favoriteActions = (
+      <>
+        <button type="button" onClick={saveFavoriteAction}>Save favorite</button>
+        <button type="button" onClick={() => removeFavorite(favoriteId)}>Remove favorite</button>
+      </>
+    );
+  } else {
+    favoriteActions = (
+      <button type="button" onClick={saveFavoriteAction}>Add to favorite</button>
+    );
+  }
+
+  return (
+    <div className="form vertical">
+      <div className="field">
+        <label>Host</label>
+        <input value={host} onChange={(e) => setHost(e.target.value)} type="text" placeholder="127.0.0.1" />
+      </div>
+      <div className="field">
+        <label>Port</label>
+        <input value={port} onChange={(e) => setPort(e.target.value)} type="text" placeholder="6379" />
+      </div>
+      <div className="buttons">
+        <button type="button" onClick={() => onConnect(host, port)}>Connect</button>
+        { favoriteActions }
+      </div>
     </div>
-    <div className="field">
-      <label>Port</label>
-      <input type="text" placeholder="6379" />
-    </div>
-    <div className="buttons">
-      <button type="button">Connect</button>
-      <button type="button">Add to favorite</button>
-    </div>
-  </div>
-);
+  );
+};
 
 export default NewConnection;
