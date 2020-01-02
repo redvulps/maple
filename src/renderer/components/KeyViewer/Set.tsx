d36e081a507e47cdaee1f808645b62272542de3a
Fabio Pereira <fabopereira@gmail.com>
2020-01-02 07:35:51 -0300

    Update set view component

--- src/renderer/components/KeyViewer/Set.tsx
+++ src/renderer/components/KeyViewer/Set.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-const Set = () => (
-  <div>Set</div>
-);
+import { IBaseTypeProps } from "./IBaseTypeProps";
+import Footer from "./Footer";
+
+const Set = ({ redisInstance, currentDatabase, currentKey }: IBaseTypeProps) => {
+  const [keyValue, setKeyValue] = useState<string[]>([]);
+  const [keyEncoding, setKeyEncoding] = useState("");
+  const [memberValue, setMemberValue] = useState("");
+  const [keyLoaded, setKeyLoaded] = useState(false);
+
+  useEffect(() => {
+    redisInstance
+      .multi()
+      .select(currentDatabase)
+      .object("encoding", currentKey.path, (err, result) => {
+        if (err) {
+          // TODO: Add error handler
+        } else {
+          setKeyEncoding(result);
+        }
+      })
+      .smembers(currentKey.path, (err, result) => {
+        if (err) {
+          // TODO: Add error handler
+        } else {
+          setKeyValue(result);
+          setKeyLoaded(true);
+        }
+      })
+      .exec();
+  }, []);
+
+  const renderResult = () => {
+    const memberList = keyValue.map((value, index) => (
+      <div key={index} onClick={() => setMemberValue(value)}>
+        {value.substring(0, 100)}
+      </div>
+    ));
+
+    return (
+      <div>
+        <div>
+          {memberList}
+        </div>
+        <div>
+          {memberValue}
+        </div>
+      </div>
+    );
+  };
+
+  return (
+    <div>
+      <div>{keyLoaded ? renderResult() : "Loading..."}</div>
+      <Footer lengthType="members" length={keyValue.length} keyEncoding={keyEncoding} />
+    </div>
+  );
+};
 
 export default Set;
