22264febfd355c4339ab3d5142250bb329a43e27
Fabio Pereira <fabopereira@gmail.com>
2020-01-02 07:39:40 -0300

    Update key viewer base

--- src/renderer/components/KeyViewer/index.tsx
+++ src/renderer/components/KeyViewer/index.tsx
@@ -1,34 +1,64 @@
 import React, { useState, useEffect } from 'react';
-import Redis from 'redis';
+import { RedisClient } from 'redis';
 
 import { IRedisKey } from '../KeyTree';
+import { IBaseTypeProps } from './IBaseTypeProps';
+
+import Set from './Set';
+import ZSet from './ZSet';
+import List from './List';
+import Hash from './Hash';
+import String from './String';
+
+const viewComponent: { [key: string]: (props: IBaseTypeProps) => JSX.Element } = {
+  string: String,
+  hash: Hash,
+  list: List,
+  zset: ZSet,
+  set: Set,
+};
 
 export interface IKeyViewerProps {
-  redisInstance: Redis.RedisClient;
+  redisInstance: RedisClient;
   currentDatabase: number;
   currentKey: IRedisKey | null;
 }
 
-const KeyViewer = ({currentKey}: IKeyViewerProps) => {
-  const [keyView, setKeyView] = useState<JSX.Element | null>(null);
+const KeyViewer = ({currentKey, redisInstance, currentDatabase}: IKeyViewerProps) => {
+  const [keyView, setKeyView] = useState<string | null>(null);
 
   const renderKey = () => {
     if (keyView) {
-      const Component = keyView;
+      const Component: any = viewComponent[keyView];
 
       return (
-        <Component />
-      );
-    } else {
-      return (
-        'Loading...'
+        <Component
+          currentKey={currentKey}
+          redisInstance={redisInstance}
+          currentDatabase={currentDatabase}
+        />
       );
     }
+
+    return (
+      'Loading...'
+    );
   };
 
   useEffect(() => {
     if (currentKey) {
-
+      redisInstance
+        .multi()
+        .select(currentDatabase)
+        .type(currentKey.path, (err, result: string) => {
+          if (err) {
+            console.warn(err);
+            // TODO: Add error handling
+          } else {
+            setKeyView(result);
+          }
+        })
+        .exec();
     }
   }, [currentKey]);
 
