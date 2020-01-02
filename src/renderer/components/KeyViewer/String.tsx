e129ce5e77d6fc7ee73b30e6199e4dd38e8fbaa3
Fabio Pereira <fabopereira@gmail.com>
2020-01-02 07:33:08 -0300

    Update string view component

--- src/renderer/components/KeyViewer/String.tsx
+++ src/renderer/components/KeyViewer/String.tsx
@@ -1,7 +1,40 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { IBaseTypeProps } from "./IBaseTypeProps";
+import Footer from "./Footer";
 
-const String = () => (
-  <div>String</div>
-);
+const String = ({ redisInstance, currentDatabase, currentKey }: IBaseTypeProps) => {
+  const [keyValue, setKeyValue] = useState("");
+  const [keyEncoding, setKeyEncoding] = useState("");
+  const [keyLoaded, setKeyLoaded] = useState(false);
+
+  useEffect(() => {
+    redisInstance
+      .multi()
+      .select(currentDatabase)
+      .object("encoding", currentKey.path, (err, result) => {
+        if (err) {
+          // TODO: Add error handler
+        } else {
+          setKeyEncoding(result);
+        }
+      })
+      .get(currentKey.path, (err, result: string) => {
+        if (err) {
+          // TODO: Add error handler
+        } else {
+          setKeyLoaded(true);
+          setKeyValue(result);
+        }
+      })
+      .exec();
+  }, []);
+
+  return (
+    <div>
+      <div>{keyLoaded ? keyValue : "Loading..."}</div>
+      <Footer lengthType="bytes" length={Buffer.byteLength(keyValue, "utf-8")} keyEncoding={keyEncoding} />
+    </div>
+  );
+};
 
 export default String;
